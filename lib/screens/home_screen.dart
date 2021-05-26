d787f09984d2b3f1ca548cda1f9f724e3072dc52
ps6067966 <ps6067966@gmail.com>
2021-05-26 21:36:31 +0530

    ps

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
