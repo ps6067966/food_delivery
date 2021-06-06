149f946722eb43e7d1df777f09f7b1b4b13b9555
KeneePatel <patelkenee2804@gmail.com>
2021-06-06 16:26:01 +0530

    Completed the top part of profile picture and bg

--- lib/pages/home_pages/menu/drawer_menus/my_account.dart
+++ lib/pages/home_pages/menu/drawer_menus/my_account.dart
@@ -6,25 +6,59 @@ class MyAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    final double imgheight = 225;
+    final double pfpsize = 150;
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 10.0,
-        leading: IconButton(
-          icon: Icon(
-            Icons.chevron_left_outlined,
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: imgheight + pfpsize/2,
+            width: width,
+            child: Stack(
+              children: <Widget>[
+                Image.network(
+                  'https://images.unsplash.com/photo-1505935428862-770b6f24f629?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZvb2QlMjBwbGF0ZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+                  width: width,
+                  height: imgheight,
+                  fit: BoxFit.fill,
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: width / 2 - 75,
+                  child: Container(
+                    height: pfpsize,
+                    width: pfpsize,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: CustomTheme.primaryColor,
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            'https://images.unsplash.com/photo-1474978528675-4a50a4508dc3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        border: Border.all(
+                          width: 8,
+                          color: Colors.white,
+                        )),
+                  ),
+                )
+              ],
+            ),
           ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: Text(
-          "My Account",
-          style: TextStyle(
-            color: CustomTheme.primaryColor,
+          SizedBox(
+            height: 15,
           ),
-        ),
-        centerTitle: true,
+          Text(
+            'Kenee Patel',
+            style: CustomTheme.title1.override(
+              fontFamily: 'Poppins',
+              fontSize: 20
+            )
+          ),
+        ],
       ),
     );
   }
